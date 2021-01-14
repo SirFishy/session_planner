816149cb13ca917aa9045af2463f41453a2033d6
Kristian Fischer <khfishy@gmail.com>
2021-01-14 15:24:27 -0500

    configure mongoose to not use deprecated functions

--- server/lib/app.ts
+++ server/lib/app.ts
@@ -1,15 +1,22 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { Routes } from "./routes/crmRoutes";
+import * as mongoose from "mongoose";
+import { Routes } from "./routes/routes";
 
 class App {
   public app: express.Application;
   public routePrv: Routes = new Routes();
+  public mongoUrl: string = "mongodb://localhost/SPdb";
 
   constructor() {
     this.app = express();
     this.config();
     this.routePrv.routes(this.app);
+    this.mongoSetup().then((r) => console.log("Database is connected."));
+  }
+
+  private async mongoSetup(): Promise<void> {
+    await mongoose.connect(this.mongoUrl);
   }
 
   private config(): void {
